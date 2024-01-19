76f5d20d36eb996814996ac24b72286d90408534
sarah <sarah.teoh752@gmail.com>
2024-01-19 22:35:29 +0800

    feat: migrate and make verification history model

---
+++ app/Models/VerificationHistory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\FileType;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class VerificationHistory extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'file_type',
+        'result',
+        'timestamp'
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'file_type' => FileType::class
+    ];
+
+    /**
+     * Get the user that verified the file.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
