a24ab4b05ebd2e672d5718ae92241eb9da5466ed
sarah <sarah.teoh752@gmail.com>
2024-01-31 10:47:09 +0800

    test: always accept json middleware

---
+++ tests/Unit/Http/Middleware/AlwaysAcceptJsonTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Middleware\AlwaysAcceptJson;
+use Illuminate\Http\Request;
+
+it('sets the Accept header to application/json', function () {
+    $request = new Request();
+    $middleware = new AlwaysAcceptJson();
+
+    $middleware->handle($request, function ($req) {
+        expect($req->headers->get('Accept'))->toBe('application/json');
+    });
+});
+
+it('overrides the Accept header if it is already set', function () {
+    $request = new Request();
+    $request->headers->set('Accept', 'text/html');
+    $middleware = new AlwaysAcceptJson();
+
+    $middleware->handle($request, function ($req) {
+        expect($req->headers->get('Accept'))->toBe('application/json');
+    });
+});
